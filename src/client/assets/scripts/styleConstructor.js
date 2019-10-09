37fc2db9f58e6cf4dbde73d6b92ad54f877ba0cb
Andriy <lodochnikkot@gmail.com>
2019-10-09 21:31:51 +0300

    New property for style constructor

--- src/client/assets/scripts/styleConstructor.js
+++ src/client/assets/scripts/styleConstructor.js
@@ -20,5 +20,15 @@ var propertiesCollection = {
         name:"Рамка",
         value:"border",
         hint:"[ширина] [тип] [цвет]"
+    },
+    background:{
+        name:"Фон",
+        value:"background",
+        hint:"[параметры фона...]"
+    },
+    background_attachment:{
+        name:"Вложение фона",
+        value:"background-attachment",
+        hint:"[ fixed | scroll | inherit]"
     }
 }
