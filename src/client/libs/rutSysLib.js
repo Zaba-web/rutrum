888e2bcb8eb644ada416e573446241ac524b3594
Andriy <lodochnikkot@gmail.com>
2019-10-23 16:42:07 +0300

    Change export method

--- src/client/libs/rutSysLib.js
+++ src/client/libs/rutSysLib.js
@@ -353,13 +353,22 @@ class AllCSSPropertiesWriter extends OptionListWriter{
 class ClassListWriter extends OptionListWriter{
     constructor(target){
         super(target);
+        this.bootstrapClasses = [
+            "col-1","col-2","col-3","col-4","col-5","col-6","col-7","col-8","col-9","col-10","col-11","col-12",
+            "col-sm-1","col-sm-2","col-sm-3","col-sm-4","col-sm-5","col-sm-6","col-sm-7","col-sm-8","col-sm-9","col-sm-10","col-sm-11","col-sm-12",
+            "col-md-1","col-md-2","col-md-3","col-md-4","col-md-5","col-md-6","col-md-7","col-md-8","col-md-9","col-md-10","col-md-11","col-md-12",
+            "col-lg-1","col-lg-2","col-lg-3","col-lg-4","col-lg-5","col-lg-6","col-lg-7","col-lg-8","col-lg-9","col-lg-10","col-lg-11","col-lg-12"];
     }
     
     write(){
+        $(this.target).append("<option disabled>---------</option><option disabled>Ваши классы: </option><option disabled>---------</option>");
         for(let key in window.mediaContainer.styles.classes){
             $(this.target).append("<option value='"+window.mediaContainer.styles.classes[key].name+"'>"+window.mediaContainer.styles.classes[key].name+"</option>");
         }
         $(this.target).append("<option disabled>---------</option><option disabled>Классы Bootstrap: </option><option disabled>---------</option>");
+        for(let i = this.bootstrapClasses.length-1; i >= 0; i--){
+            $(this.target).append("<option value='"+this.bootstrapClasses[i]+"'>"+this.bootstrapClasses[i]+"</option>");
+        }
     }
     
 }
@@ -840,7 +849,7 @@ class Exporter{
     
     constructor(){
         this.fs = require("fs");
-        this.exportPath = window.projectDir+"\\"+"Website";
+        this.exportPath = window.projectDir+"/"+"Website";
     }
     
     export(){
@@ -852,7 +861,7 @@ class Exporter{
         this.buildPages();
         
         let gui = require('nw.gui');
-        gui.Shell.openExternal(this.exportPath);
+        gui.Shell.openItem(this.exportPath);
     }
     
     createTree(){
